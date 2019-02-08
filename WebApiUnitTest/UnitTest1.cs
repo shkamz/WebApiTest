7d820565b4ed2d68c7bcb4a78e0881b82bf64365
Shukri Armizi <shukriarmizi@gmail.com>
2019-02-08 19:46:23 +0800

    Commit All Files

--- WebApiUnitTest/UnitTest1.cs
+++ WebApiUnitTest/UnitTest1.cs
@@ -1,14 +1,37 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace WebApiUnitTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        //private readonly WebApplicationFactory<RazorPagesProject.Startup> _factory;
 
+        //public BasicTests(WebApplicationFactory<RazorPagesProject.Startup> factory)
+        //{
+        //    _factory = factory;
+        //}
+        public HttpClient _client;
+        public UnitTest1()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<IStartup>());
+            _client = server.CreateClient();
+        }
+
+        [Fact]
+        public async Task Test()
+        {
+            var response = await _client.GetAsync("/api/People/5");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.Equal("{\"author\":\"haha\",\"title\":\"Kimak\"}", responseString);
+            //Welcome pc = new Welcome();
+            //Book cpd = pc.Test("lol");
+            //Assert.Equal("lol", cpd.Author);
         }
     }
 }
